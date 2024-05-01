7ca7f461d3861b66f664875c42006fe58f1f2f9b
Lemondemones <sergej.omelchenko.02@mail.ru>
2024-05-01 19:57:02 +0300

    :bookmark: improved the UI and added more flexibility

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -32,3 +32,29 @@ const handleBlurHeader = function (): void {
 };
 
 window.addEventListener("scroll", handleBlurHeader);
+
+const scrollUp = function (): void {
+    const scrollUp = document.getElementById("scroll-up");
+    window.scrollY >= 350
+        ? scrollUp?.classList.add("show-scroll")
+        : scrollUp?.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
+
+const sections = document.querySelectorAll("section[id]");
+const scrollActive = (): void => {
+    const scrollDown = window.scrollY;
+
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute("id"),
+            sectionClass = document.querySelector(".nav__menu a[href*='" + sectionId + "']");
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionClass?.classList.add("active-link");
+        } else {
+            sectionClass?.classList.remove("active-link");
+        }
+    });
+};
+window.addEventListener("scroll", scrollActive);
